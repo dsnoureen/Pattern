4e8a962b109310f93c2a3647199ad4bb5ab1bf1c
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-29 12:52:17 +0600

    pattern 2 done

--- pattern2.c
+++ pattern2.c
@@ -6,5 +6,15 @@ int main(){
     3 3 3
     4 4 4 4
     */
+   int n, row, col;
+    scanf("%d", &n);
+    for (int row = 1; row <= n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            printf("%d ", row);
+        }
+        printf("\n");
+    }
     return 0;
 }
