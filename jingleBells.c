f269cb62bf2659fdba7566a132eeabd83849cfe4
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-29 12:38:54 +0600

    1st pattern doing

---
+++ jingleBells.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<(5+(n+1));i++){
+        for(int j=0;j<=i;j++){
+             
+        }
+    }
+    return 0;
+}
