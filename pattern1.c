d80752fc090549a4413a07ca241a4d09abd7158c
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-29 12:49:00 +0600

    complete

--- pattern1.c
+++ pattern1.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
-int main(){
+int main()
+{
     /*
     1
     1 2
     1 2 3
     1 2 3 4
     */
-   int n,row,col;
-   for(int row=0;row<n;row++){
-    for(int col=0;col<=row;col++){
-        printf("%d ",col);
+    int n, row, col;
+    scanf("%d", &n);
+    for (int row = 1; row < n; row++)
+    {
+        for (int col = 1; col <= row; col++)
+        {
+            printf("%d ", col);
+        }
+        printf("\n");
     }
-    printf("\n");
-   }
     return 0;
 }
---
+++ pattern2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    /*
+    1
+    2 2
+    3 3 3
+    4 4 4 4
+    */
+    return 0;
+}
