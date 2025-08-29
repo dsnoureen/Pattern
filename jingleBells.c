d37cd7598849d362f80c8a49fc6582a3b71a9980
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-30 00:49:32 +0600

    christmas tree pattern

--- jingleBells.c
+++ jingleBells.c
@@ -1,29 +1,39 @@
 #include <stdio.h>
+int main()
+{
+    int n, row, col;
+    scanf("%d", &n);
+    int height = n + 5;                
+    int width = (height * 2) - 1;  
+    for (row = 1; row <= height; row++)
+    {
+        int stars = (2 * row) - 1;
+        int spaces = (width - stars) / 2;
 
-int main() {
-    int N;
-    scanf("%d", &N);
-
-    int height = N + 5;            // total rows for top
-    int max_width = (height * 2) - 1;  // maximum stars in the last row
-
-    // Print tree top
-    for (int i = 1; i <= height; i++) {
-        int stars = (2 * i) - 1;     // odd numbers: 1,3,5,...
-        int spaces = (max_width - stars) / 2;
-        
-        for (int j = 0; j < spaces; j++) printf(" ");
-        for (int j = 0; j < stars; j++) printf("*");
+        for (col = 1; col <= spaces; col++)
+        {
+            printf(" ");
+        }
+        for (col = 1; col <= stars; col++)
+        {
+            printf("*");
+        }
         printf("\n");
     }
 
-    // Print trunk
-    for (int i = 0; i < 5; i++) {
-        int spaces = (max_width - N) / 2;
-        for (int j = 0; j < spaces; j++) printf(" ");
-        for (int j = 0; j < N; j++) printf("*");
+    for (row = 1; row <= 5; row++)
+    {
+        int spaces = (width - n) / 2;
+
+        for (col = 1; col <= spaces; col++)
+        {
+            printf(" ");
+        }
+        for (col = 1; col <= n; col++)
+        {
+            printf("*");
+        }
         printf("\n");
     }
-
     return 0;
 }
