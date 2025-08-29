886ee562eb9ea92e82b71477c09094a5bc47a3ba
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-30 01:14:06 +0600

    star pattern

--- jingleBells.c
+++ jingleBells.c
@@ -3,36 +3,26 @@ int main()
 {
     int n, row, col;
     scanf("%d", &n);
-    int height = n + 5;                
-    int width = (height * 2) - 1;  
+
+    int height = n + 5;                 
+    int width = (height * 2) - 1;      
     for (row = 1; row <= height; row++)
     {
-        int stars = (2 * row) - 1;
-        int spaces = (width - stars) / 2;
-
+        int stars = (2 * row) - 1;        
+        int spaces = (width - stars) / 2;  
         for (col = 1; col <= spaces; col++)
-        {
             printf(" ");
-        }
         for (col = 1; col <= stars; col++)
-        {
             printf("*");
-        }
         printf("\n");
     }
-
     for (row = 1; row <= 5; row++)
     {
-        int spaces = (width - n) / 2;
-
+        int spaces = (width - n) / 2;  
         for (col = 1; col <= spaces; col++)
-        {
             printf(" ");
-        }
         for (col = 1; col <= n; col++)
-        {
             printf("*");
-        }
         printf("\n");
     }
     return 0;
