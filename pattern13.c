755064ba446a45747afffb48daa1937cdd0f9c6f
Dewan Sadia Noureen <dewansadianoureen02@gmail.com>
2025-08-30 00:01:00 +0600

    triangle

---
+++ pattern13.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main(){
+    /*
+
+    *
+    **  
+    * *  
+    *  * 
+    *****
+
+    */
+   int n, row, col;
+    scanf("%d", &n);
+    for (int row = 1; row<=n; row++)
+    {
+        for (int col = 1; col<=n; col++)
+        {
+            if(row==col || row==n ||col==1 ){
+                printf("*");
+            }
+            else {
+                printf(" ");
+            }
+        }
+        
+        printf("\n");
+    }
+    return 0;
+}
